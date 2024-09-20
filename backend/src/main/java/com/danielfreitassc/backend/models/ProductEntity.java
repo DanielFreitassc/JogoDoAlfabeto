b70e9c6af1eef5ee9a97a2ef799676c15a130d5d
DanielFreitassc <danielfreitassc@gmail.com>
2024-09-20 09:51:02 -0300

    Feat: Criei a entidade produto

---
+++ backend/src/main/java/com/danielfreitassc/backend/models/ProductEntity.java
@@ -0,0 +1,30 @@
+package com.danielfreitassc.backend.models;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name="product")
+public class ProductEntity {
+    
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    private int quantity;
+    private  BigDecimal price;
+}
